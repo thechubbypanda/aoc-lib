95f5c9cbc33b0c42feb1a76578c90bdd02bad26a
Keval Kapdee <keval@thechubbypanda.net>
2021-12-03 19:47:27 +0000

    Added test and input caching

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
-use reqwest::header::COOKIE;
-use std::{fmt::Display, time::Instant};
+use std::fmt::Display;
+use std::fs::File;
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::time::Instant;
 
-const SESSION_COOKIE_FILE: &str = ".aoc.cookie";
+use reqwest::header::COOKIE;
 
 fn read_session_cookie() -> String {
     let filename = home::home_dir()
         .expect("Failed to find home directory")
-        .join(SESSION_COOKIE_FILE);
+        .join(".aoc.cookie");
     println!("Reading session cookie from file: {:?}", filename);
     std::fs::read_to_string(filename)
         .expect("Failed to read from session file")
@@ -14,30 +17,88 @@ fn read_session_cookie() -> String {
         .to_string()
 }
 
-pub fn download_input(year: u16, day: u8) -> Vec<String> {
-    let url = format!("https://adventofcode.com/{}/day/{}/input", year, day);
-    let cookie = format!("session={}", read_session_cookie());
-    reqwest::blocking::Client::new()
-        .get(&url)
-        .header(COOKIE, cookie)
-        .send()
-        .expect("Failed to send request")
-        .error_for_status()
-        .expect("Got error response from server")
-        .text()
-        .expect("Failed to parse text from response")
-        .lines()
-        .map(String::from)
-        .collect()
-}
-
-pub fn run<F, R>(code: F, year: u16, day: u8, part: u8)
+pub fn run<F, R>(year: u16, day: u8, code: F)
 where
     F: Fn(Vec<String>) -> R,
     R: Display,
 {
-    let input = download_input(year, day);
+    let client = Client::new(year);
+    let input = client.get_input(day).lines().map(String::from).collect();
     let timer = Instant::now();
-    println!("Part {} output: {}", part, code(input));
-    println!("Part {} time: {:?}", part, timer.elapsed());
+    println!("Output: {}", code(input));
+    println!("Time: {:?}", timer.elapsed());
+}
+
+pub fn test<F, R>(_: u16, _: u8, code: F)
+where
+    F: Fn(Vec<String>) -> R,
+    R: Display,
+{
+    let input = std::fs::read_to_string("input/test.in")
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect();
+    let timer = Instant::now();
+    println!("Output: {}", code(input));
+    println!("Time: {:?}", timer.elapsed());
+}
+
+struct Client {
+    year: u16,
+    session_token: String,
+    client: reqwest::blocking::Client,
+    input_dir: PathBuf,
+}
+
+impl Client {
+    pub fn new(year: u16) -> Self {
+        let input_dir = Path::new("input").to_path_buf();
+        if let Err(e) = std::fs::create_dir(input_dir.clone()) {
+            println!("Didn't create input dir: {}", e.to_string());
+        }
+        Self {
+            input_dir,
+            year,
+            session_token: read_session_cookie(),
+            client: reqwest::blocking::Client::new(),
+        }
+    }
+
+    fn get_day_path(&self, day: u8) -> PathBuf {
+        self.input_dir.join(format!("{}.in", day))
+    }
+
+    pub fn get_input(&self, day: u8) -> String {
+        if let Ok(input) = std::fs::read_to_string(self.get_day_path(day)) {
+            return input;
+        }
+
+        let input = self.download_input(day);
+        self.cache_input(day, &input);
+
+        input
+    }
+
+    fn cache_input(&self, day: u8, input: &str) {
+        File::create(self.get_day_path(day))
+            .expect("Failed to create input file")
+            .write_all(input.as_bytes())
+            .expect("Failed to write input file");
+    }
+
+    fn download_input(&self, day: u8) -> String {
+        self.client
+            .get(&format!(
+                "https://adventofcode.com/{}/day/{}/input",
+                self.year, day
+            ))
+            .header(COOKIE, format!("session={}", self.session_token))
+            .send()
+            .expect("Failed to sent input request to server")
+            .error_for_status()
+            .expect("Got error from server")
+            .text()
+            .expect("Failed to parse response from server")
+    }
 }
