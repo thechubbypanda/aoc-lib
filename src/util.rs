aca29478c86240519b4501c22bc2fa3cdb3eff85
Keval Kapdee <keval@thechubbypanda.net>
2021-12-04 09:08:26 +0000

    Add parse_strings

--- src/util.rs
+++ src/util.rs
@@ -1,12 +1,21 @@
-pub fn to_lines(input: String) -> Vec<String> {
+use std::str::FromStr;
+
+pub fn to_lines(input: &String) -> Vec<String> {
     input.lines().map(String::from).collect()
+}
+
+pub fn parse_strings<T>(input: &[String]) -> Result<Vec<T>, T::Err>
+where
+    T: FromStr,
+{
+    input.iter().map(|s| s.parse()).collect()
 }
 
 pub fn transpose<T>(input: &Vec<Vec<T>>) -> Vec<Vec<T>>
 where
     T: Copy,
 {
-    assert!(input.iter().all(|sub| sub.len() == input[0].len()));
+    assert!(input.windows(2).all(|w| w[0].len() == w[1].len()));
     (0..input[0].len())
         .map(|i| input.iter().map(|line| line[i]).collect())
         .collect()
